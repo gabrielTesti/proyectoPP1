2e8066c1cd7452265295c7f0a4343050fbe524fc
gabrielTesti <gabrieltesti22@gmail.com>
2024-11-12 18:44:48 -0300

    css

--- frontendPP1/src/app/app.module.ts
+++ frontendPP1/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductoComponent } from './components/producto/producto.component';
 import {MatTableModule} from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
@@ -23,7 +24,8 @@ import { MatInputModule } from '@angular/material/input';
     BrowserAnimationsModule,
     HttpClientModule,
     MatTableModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
